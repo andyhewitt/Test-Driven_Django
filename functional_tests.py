78b911918e9c55b490734d688a3d6d7d91ca97d3
andyhewitt <andyhewitt1987@gmail.com>
2018-11-19 21:11:33 +0900

    add chromedrive

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000/')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_late(self):
+        # The test function goes to checl the homw page of a new online to-do app.
+        self.browser.get('http://127.0.0.1:8000/')
+
+        # the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # test app is invited to enter a to-do item straight away
+        # she types ....
+
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
